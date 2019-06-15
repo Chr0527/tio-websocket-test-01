c6e83eb790a39faf707dd8998919abc00cb432e4
chr0527 <1015670600@qq.com>
2019-06-15 15:08:10 +0800

    Initial commit

---
+++ src/main/java/com/chryl/server/Const.java
@@ -0,0 +1,11 @@
+package com.chryl.server;
+
+/**
+ * Created By Chr on 2019/6/15.
+ */
+public class Const {
+    /**
+     * 用于群聊的group id
+     */
+    public static final String GROUP_ID = "showcase-websocket";
+}
