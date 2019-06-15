e749f03a4b28bee8dc0840d2a180467fd2995fc4
chr0527 <1015670600@qq.com>
2019-06-15 15:15:17 +0800

    init

--- src/main/java/com/chryl/TioWebsocketTest01Application.java
+++ src/main/java/com/chryl/TioWebsocketTest01Application.java
@@ -1,13 +1,16 @@
 package com.chryl;
 
+import com.chryl.server.start.ShowcaseWebsocketStarter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TioWebsocketTest01Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TioWebsocketTest01Application.class, args);
-	}
+    public static void main(String[] args) throws Exception {
+        ShowcaseWebsocketStarter.start();
+
+        SpringApplication.run(TioWebsocketTest01Application.class, args);
+    }
 
 }
